668b97a39027025e94c38f68ecd70655be4812d3
Fernando Contreras <fernando.cont.glez@gmail.com>
2023-06-02 19:04:22 -0600

    Se agrega parte de modal agrega tarjeta

--- screens/Tabs/TabTarjetas.jsx
+++ screens/Tabs/TabTarjetas.jsx
@@ -1,36 +1,87 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Divider, List, ListItem, Card } from '@ui-kitten/components';
+import React, { useState } from 'react';
+import { StyleSheet , Modal, View, SafeAreaView} from 'react-native';
+import { Divider, List, ListItem, Card, Button, Layout, Text, TopNavigation, TopNavigationAction, Input } from '@ui-kitten/components';
+
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import { useSelector } from 'react-redux';
 import TemplateMenu from '../TemplateMenu';
 
 
-const data = new Array(8).fill({
-  title: 'Item',
-  description: 'Description for Item',
-});
+
+
 
 const TabTarjetas = () => {
-
+    
+    const [visible, setVisible] = useState(false);
     const cards = useSelector( state => state.cards.cards)
 
-  const renderItem = ({ item, index }) => (
-    <ListItem
-      title={`${item.name}`}
-      description={`${item.name}`}
-    />
-  );
+    const [valueName, setValueName] = useState('');
+    const [valueDescprition, setValueDescription] = useState('');
+    
+    const iconAddCard= (props) => (
+        <FontAwesome5 name='plus-circle' color={'#ffffff'} size={20}/>
+    )
+    const iconGoBack= () => (
+        <FontAwesome5 name='chevron-left' color={'#0469ee'} size={20}/>
+    )
+    const renderItem = ({ item, index }) => (
+        <ListItem
+        title={`${item.name}`}
+        description={`${item.name}`}
+        />
+    );
+
+    const renderTopNavigation = () => (
+        <TopNavigationAction icon={iconGoBack} onPress={() => setVisible(false)} />
+    )
+
+  const pressAddSpent = () => {
+    console.log("Se da clic al add")
+    setVisible(true)
+
+  }
 
   return (
         <TemplateMenu TabSelected={2}>
-            <Card>
-                <List
-                data={cards}
-                ItemSeparatorComponent={Divider}
-                renderItem={renderItem}
-                />
-            </Card>
+            <Layout 
+                level='1'
+            >
+                <Card>
+                    <List
+                    data={cards}
+                    ItemSeparatorComponent={Divider}
+                    renderItem={renderItem}
+                    />
+                </Card>
+                <Button accessoryLeft={iconAddCard} onPress={pressAddSpent}> Añadir Tarjeta</Button>
+
+                <Modal visible= {visible}>
+                    <SafeAreaView  style={{flex:1, paddingTop: Platform.OS==="android"?25:0}}>
+                        <View>
+                            <TopNavigation 
+                                accessoryLeft={renderTopNavigation}
+                                title='Agregar Tarjeta de Crédito'
+                            />
+                            <Text category='h6'>Nombre:</Text>
+                            <Input 
+                                placeholder='Ingresa el nombre'
+                                value={valueName}
+                                onChangeText={newValue=>setValueName(newValue)}
+                            />
+                            <Text category='h6'>Descripción:</Text>
+                            <Input 
+                                placeholder='Ingresa la descripción'
+                                value={valueDescprition}
+                                onChangeText={newValue=>setValueDescription(newValue)}
+                            />
+                            <View style={styles.buttons}>
+                                <Button onPress={()=> console.log("Se da clic en guardar,",valueName)} > Guardar</Button>
+                            </View>
+                        </View> 
+                    </SafeAreaView>
+                </Modal>
+            </Layout>
         </TemplateMenu>
   );
 };
@@ -39,6 +90,10 @@ const styles = StyleSheet.create({
   container: {
     maxHeight: 20,
   },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center'
+  }
 });
 
 export default TabTarjetas
